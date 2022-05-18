5afd5365482c9c2ff4a331ce2dab63bd0589719f
tsots <mosalatsotetsi@gmail.com>
2022-05-19 00:36:56 +0200

    starting gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/heiya",
+  assetPrefix: "/heiya",
 }
 
 module.exports = nextConfig
