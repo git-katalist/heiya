f5e70dac352d8ff14b8ca9782b83b7480a921163
git-katalist <79943903+git-katalist@users.noreply.github.com>
2022-05-18 23:28:39 +0000

    Deploying to gh-pages from @ git-katalist/heiya@909fa6f153b077d747c58708393ad4146491bca4 🚀

--- _next/static/Gchdi8uYseRXNUmxbEfcD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-26d1392dde37b887.js","static/css/149b18973e5508c7.css","static/chunks/pages/index-15fb1856d230de1c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
